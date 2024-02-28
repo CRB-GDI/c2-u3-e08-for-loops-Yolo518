a1ef18ac68fb4f72ec61f34a8129f68cd20dfaa4
Yolo518 <145461025+Yolo518@users.noreply.github.com>
2024-02-28 10:19:32 -0500

    got to loop

---
+++ Exercise8/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 0;
+            Console.WriteLine("pleas enter a max number ");
+            maxNumber=int.Parse(Console.ReadLine());
+            for(int i = 2; i <= maxNumber; i++)
+            {
+                //find all even numbers with in the max number
+                if (maxNumber % i == 0) 
+                {
+                    Console.WriteLine(maxNumber % i);
+                }
+
+            }
+            
+        }
+    }
+}
